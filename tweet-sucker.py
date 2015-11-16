c5de404b7a3fcb886152f34d73c7adf33e1bd08e
Chris Jepeway <jepeway@blasted-heath.com>
2015-11-15 22:09:54 -0500

    Use reentrantmethod module

--- tweet-sucker.py
+++ tweet-sucker.py
@@ -6,25 +6,7 @@ import tweepy, json
 import traceback
 import argparse, pprint
 from kafkatweetstore import KafkaTweetStore
-
-class ReentrantMethod(object):
-   """
-   Make a given object's method be re-entrant.
-   """
-
-   orig_meth = None
-   obj = None
-   def __init__(self, obj, meth):
-      self.orig_meth = meth
-      self.obj = obj
-      setattr(self.obj, self.orig_meth.func_name, self._wrap)
-   def _noop(self, *args, **kwargs):
-      pass
-   def _wrap(self, *args, **kwargs):
-      setattr(self.obj, self.orig_meth.func_name, self._noop)
-      self.orig_meth(*args, **kwargs)
-      setattr(self.obj, self.orig_meth.func_name, self._wrap)
-
+from reentrantmethod import ReentrantMethod
 
 class TweetStore(object):
    """
