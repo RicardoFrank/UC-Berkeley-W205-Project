1eff5b1487a085fa603c71e2e16d2b01bd811b8c
Chris Jepeway <jepeway@blasted-heath.com>
2015-11-15 22:07:26 -0500

    Start moving code into reentrantmethod.py

---
+++ reentrantmethod.py
@@ -0,0 +1,17 @@
+class ReentrantMethod(object):
+   """
+   Make a given object's method be re-entrant.
+   """
+
+   orig_meth = None
+   obj = None
+   def __init__(self, obj, meth):
+      self.orig_meth = meth
+      self.obj = obj
+      setattr(self.obj, self.orig_meth.func_name, self._wrap)
+   def _noop(self, *args, **kwargs):
+      pass
+   def _wrap(self, *args, **kwargs):
+      setattr(self.obj, self.orig_meth.func_name, self._noop)
+      self.orig_meth(*args, **kwargs)
+      setattr(self.obj, self.orig_meth.func_name, self._wrap)
