93fa7d17bde383dee15c7e8eeac156d4b8051841
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-11 13:51:02 -0500

    Improve tweet text pre-processing

--- python/spark/block-harassers.py
+++ python/spark/block-harassers.py
@@ -1,5 +1,5 @@
 import sys, argparse
-import argparse, pprint, json
+import json, re
 import pprint
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -31,9 +31,20 @@ if __name__ == "__main__":
 
    harassing_tweets.count().pprint()
    harassing_tweets.map(
-      lambda s: json.loads(s[1])['text']	# pluck out tweet's text
+      # xform to dicts
+      lambda js: json.loads(js[1])
+   ).filter(
+      # english only
+      lambda tweet: tweet['lang'] == 'en'
    ).map(
-      lambda t: t.encode('ascii','ignore')	# pprint() can only handle ascii, it seems
+      # pluck out tweet's text & downcase it
+      lambda tweet: tweet['text'].lower()
+   ).map(
+      # kill punctuation, except for @mentions and #hashtags and spaces
+      lambda txt: re.sub("[^\w\s@#]+", '', txt)
+   ).map(
+      # pprint() can only handle ascii, it seems
+      lambda txt: txt.encode('ascii','ignore')
    ).pprint()
 
    ssc.start()
