46e5db681fb294564536501922871d324e470b8c
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-11 08:11:33 -0500

    Debug & fix arg parsing, such as it is

--- python/spark/block-harassers.py
+++ python/spark/block-harassers.py
@@ -1,13 +1,17 @@
 import sys, argparse
+import pprint
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+
+
+import argparse, pprint
 
 if __name__ == "__main__":
    p = argparse.ArgumentParser(
       description='Spark job to monitor tweets and classify them as harassers'
       , formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-   p.add_argument('--broker', dest='bk_endpt', nargs=1, metavar='ENDPOINT'
+   p.add_argument('--broker', dest='bk_endpt', required=True, metavar='ENDPOINT'
 		  , help='broker endpoint for kafka store')
    topicDef = 'tweets'
    p.add_argument('--topic', dest='topic'
@@ -18,7 +22,9 @@ if __name__ == "__main__":
    ssc = StreamingContext(sc, 10)
    ssc.checkpoint("file:///tmp/checkpointing")
 
-   ks = KafkaUtils.createDirectStream(ssc, [ 'tweets', 'harassers' ], {"metadata.broker.list": [ args.bk_endpt ]})
+   pp = pprint.PrettyPrinter(indent=4)
+   pp.pprint(args.bk_endpt)
+   ks = KafkaUtils.createDirectStream(ssc, [ 'tweets', 'harassers' ], { "metadata.broker.list": args.bk_endpt })
    ks.pprint()
 
    ssc.start()
