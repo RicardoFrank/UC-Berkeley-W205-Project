c54807d2f8ffb3ea5b90d57ac341f1d25e4c748b
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-13 00:44:29 -0500

    Handle shutdown, signals, & tweepy exceptions

--- python/backends/block.py
+++ python/backends/block.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import sys, os
+import sys, os, signal
 import tweepy, json
 import argparse, pprint
 
@@ -24,13 +24,25 @@ if __name__ == '__main__':
    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
    auth.set_access_token(access_token, access_token_secret)
    api = tweepy.API(auth_handler=auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+   me = api.me().screen_name
 
    k = KafkaTransceiver(args.bk_endpt)
 
-   me = api.me().screen_name
+   def quit(signum, frame):
+      k.close()
+      exit()
+   signal.signal(signal.SIGINT, quit)
+   signal.signal(signal.SIGTERM, quit)
+
    while True:
-      m = json.loads(k.recv(args.topic))
-      api.create_block(m['author'])
-      print('@%s has blocked @%s for tweeting "%s"' % (me, m['author'], m['text']))
+      m = k.recv(args.topic)
+      m = json.loads(m)
+      try:
+         api.create_block(m['author'])
+         print('@%s has blocked @%s for tweeting "%s"' % (me, m['author'], m['text']))
+      except:
+         print("Error blocking @%s: %s" % (m['author'], sys.exc_info()[0]))
+   quit()
+
 
 # vim: expandtab shiftwidth=3 softtabstop=3 tabstop=3
