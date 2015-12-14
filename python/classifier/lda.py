bddf1cc90c127d396dacd6e08d112e5d6175259b
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-14 15:59:43 -0500

    Fix a few syntax errors

--- python/classifier/lda.py
+++ python/classifier/lda.py
@@ -9,7 +9,7 @@ class LDATweetClassifier(TweetClassifier):
 
     def __init__(self, sc = None):
         print('INITIALIZING ' + type(self).__name__)
-        self.model = LdaModel(corpus=None, num_topics=100, id2word=None, distributed=False, chunksize=2000, passes=1, update_every=1, alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50, gamma_threshold=0.001, minimum_probability=0.01
+        self.model = LdaModel(corpus=None, num_topics=100, id2word=None, distributed=False, chunksize=2000, passes=1, update_every=1, alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50, gamma_threshold=0.001, minimum_probability=0.01)
         self.nInstances += 1
         self.serno = self.nInstances
         pass
@@ -17,7 +17,7 @@ class LDATweetClassifier(TweetClassifier):
     def isHarassingTweet(self, txt):
         doc_lda = self.model[txt]
         print(doc_lda)
-        if doc_lda > 0.15
+        if doc_lda > 0.15:
           print ("    HARASSING tweet '" + txt + "'")
           return True
 
