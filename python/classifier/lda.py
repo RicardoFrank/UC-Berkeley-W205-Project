7532cb3e698bf5168841b33d3ce07b535aedf72f
letslego <amitkarmakar@ischool.berkeley.edu>
2015-12-14 18:16:32 -0500

    Changes to LDA

--- python/classifier/lda.py
+++ python/classifier/lda.py
@@ -1,21 +1,33 @@
 import random
+import logging, gensim, bz2
 
-
+from stop_words import get_stop_words
 from gensim.models.ldamodel import LdaModel
+from gensim import corpora
 from classifier.base import TweetClassifier
+from gensim.corpora import TextCorpus, MmCorpus, Dictionary
 
 class LDATweetClassifier(TweetClassifier):
     nInstances = 0
 
     def __init__(self, sc = None):
         print('INITIALIZING ' + type(self).__name__)
-        self.model = LdaModel(corpus=None, num_topics=100, id2word=None, distributed=False, chunksize=2000, passes=1, update_every=1, alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50, gamma_threshold=0.001, minimum_probability=0.01)
-        self.nInstances += 1
+	self.nInstances += 1
         self.serno = self.nInstances
         pass
 
     def isHarassingTweet(self, txt):
-        doc_lda = self.model[txt]
+	if mycorpus is None:
+	  d = gensim.corpora.dictionary.Dictionary(['this is an harassing tweet, dammit'.lower().split()])
+	  mycorpus = [d.doc2bow('this is an harassing tweet'.lower())]
+	  mycorpus = dictionary.doc2bow(txt.lower().split(),allow_update=True)
+        print("isHarassing begin")	
+	mycorpus = mycorpus.add_documents(txt.lower(),prune_at=2000000) 
+	
+	if lda is None:
+	  lda = LdaModel(corpus=mycorpus, num_topics=100, id2word=None, distributed=False, chunksize=2000, passes=1, update_every=1, alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50, gamma_threshold=0.001, minimum_probability=0.01)
+        
+	doc_lda = lda[txt.lower()]
         print(doc_lda)
         if doc_lda > 0.15:
           print ("    HARASSING tweet '" + txt + "'")
@@ -25,7 +37,7 @@ class LDATweetClassifier(TweetClassifier):
 
 
     def addHarassingTweet(self, txt):
-        self.model.update(txt)
+        self.model.update(txt.lower())
 
     def loadModel(self):
         pass
