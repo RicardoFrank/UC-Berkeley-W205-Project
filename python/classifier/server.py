fcd43917c09b3e0c8e4b2d255f0f11c5499b6d5d
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-15 05:39:12 -0500

    Print out stack traces from excpetions

--- python/classifier/server.py
+++ python/classifier/server.py
@@ -7,11 +7,21 @@ sys.path += [ os.getcwd() ]
 from classifier.rand import RandomTweetClassifier
 from classifier.key_word import KeywordTweetClassifier
 from classifier.lda import LDATweetClassifier
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+
+class handler(SimpleXMLRPCRequestHandler):
+     def _dispatch(self, method, params):
+         try: 
+             return self.server.funcs[method](*params)
+         except:
+             import traceback
+             traceback.print_exc()
+             raise
 
 #c = RandomTweetClassifier(p = 0.01)
 #c = KeywordTweetClassifier()
 c = LDATweetClassifier()
-server = SimpleXMLRPCServer(('', 6666), allow_none=True)
+server = SimpleXMLRPCServer(('', 6666), handler, allow_none=True)
 print("Listening on port 6666...")
 server.register_function(lambda txt: (print('checking: ' + txt), c.isHarassingTweet(txt))[1], "isHarassingTweet")
 server.register_function(lambda txt: (print('  adding: ' + txt), c.addHarassingTweet(txt))[1], "addHarassingTweet")
