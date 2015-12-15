987f7868ceffc3168bae8eb1c65f4ac655415c10
letslego <amitkarmakar@ischool.berkeley.edu>
2015-12-14 23:15:29 -0500

    Updating lda

--- python/classifier/lda.py
+++ python/classifier/lda.py
@@ -1,6 +1,7 @@
 import random
+import os
 import logging, gensim, bz2
-logging.basicConfig(filename='~/W205-Project/example.log',level=logging.DEBUG)
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
 from gensim.models.ldamodel import LdaModel
 from gensim import corpora
 from classifier.base import TweetClassifier
@@ -14,6 +15,7 @@ class LDATweetClassifier(TweetClassifier):
 	self.nInstances += 1
         self.serno = self.nInstances
 	self.d = None
+	self.id2word = None
 	self.model = None
 	self.tolerance = tolerance
         pass
@@ -34,20 +36,21 @@ class LDATweetClassifier(TweetClassifier):
     def addHarassingTweet(self, txt):
 	words = txt.lower().split()
 
-	if self.d is None:
+	#if self.d is None:
 	    # build dictionary
-	    self.d = gensim.corpora.dictionary.Dictionary([words])
-
+	self.d = gensim.corpora.dictionary.Dictionary([words])
+	self.d.save('/tmp/singletweet')
 	# generate bag of words
 	corpus_bow = self.d.doc2bow(words, allow_update=True)
-
-#	if (self.model is not None) and (corpus_bow is not None):
-#	    self.model.update([corpus_bow],update_every=1)
-#	else:
+	if self.id2word is None:
+	   self.id2word = gensim.corpora.Dictionary.load('/tmp/singletweet')
+	if (self.model is not None) and (corpus_bow is not None):
+	    self.model.update([corpus_bow],update_every=1)
+	else:
 	    # build model using this first tweet
-	if self.model is None:
-	     self.model = LdaModel(corpus=[corpus_bow],num_topics=10, eval_every=10,update_every=1,passes=10)
-
+	    self.model = LdaModel(corpus=None,num_topics=10,id2word=self.id2word, eval_every=10,update_every=1,passes=10)
+	os.remove('/tmp/singletweet')
+ 
     def loadModel(self):
         pass
 
