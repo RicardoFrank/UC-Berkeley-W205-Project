fc6ae7c44bfaf97c138db8dfa4a7d59d51e0b326
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-14 20:09:48 -0500

    Get LDAClassifier working...almost
    The .update() method for our LdaModel is still unhappy.
    It dies with exceptions like
    
        Fault: <Fault 1: "<type 'exceptions.IndexError'>:index 9 is out of bounds for axis 1 with size 9">

--- python/classifier/lda.py
+++ python/classifier/lda.py
@@ -1,7 +1,6 @@
 import random
 import logging, gensim, bz2
 
-from stop_words import get_stop_words
 from gensim.models.ldamodel import LdaModel
 from gensim import corpora
 from classifier.base import TweetClassifier
@@ -10,38 +9,46 @@ from gensim.corpora import TextCorpus, MmCorpus, Dictionary
 class LDATweetClassifier(TweetClassifier):
     nInstances = 0
 
-    def __init__(self, sc = None):
+    def __init__(self, tolerance = 0.4, sc = None):
         print('INITIALIZING ' + type(self).__name__)
 	self.nInstances += 1
         self.serno = self.nInstances
+	self.d = None
+	self.model = None
+	self.tolerance = tolerance
         pass
 
     def isHarassingTweet(self, txt):
-	if mycorpus is None:
-	  d = gensim.corpora.dictionary.Dictionary(['this is an harassing tweet, dammit'.lower().split()])
-	  mycorpus = [d.doc2bow('this is an harassing tweet'.lower())]
-	  mycorpus = dictionary.doc2bow(txt.lower().split(),allow_update=True)
-        print("isHarassing begin")	
-	mycorpus = mycorpus.add_documents(txt.lower(),prune_at=2000000) 
-	
-	if lda is None:
-	  lda = LdaModel(corpus=mycorpus, num_topics=100, id2word=None, distributed=False, chunksize=2000, passes=1, update_every=1, alpha='symmetric', eta=None, decay=0.5, offset=1.0, eval_every=10, iterations=50, gamma_threshold=0.001, minimum_probability=0.01)
-        
-	doc_lda = lda[txt.lower()]
+	if self.model is None:
+	    # Nothing to see here
+	    return False
+
+	bow = self.d.doc2bow(txt.split())	# bag of words
+	doc_lda = self.model[bow]
         print(doc_lda)
-        if doc_lda > 0.15:
-          print ("    HARASSING tweet '" + txt + "'")
-          return True
-
-        return False
-
+	harassing = any(_[1] > self.tolerance for _ in doc_lda)
+        if harassing:
+            print ("    HARASSING tweet '" + txt + "'")
+        return harassing
 
     def addHarassingTweet(self, txt):
-        self.model.update(txt.lower())
+	words = txt.split()
+
+	if self.d is None:
+	    # build dictionary
+	    self.d = gensim.corpora.dictionary.Dictionary([words])
+
+	# generate bag of words
+	bow = self.d.doc2bow(words, allow_update=True)
+
+	if self.model is not None:
+	    self.model.update([bow])
+	else:
+	    # build model using this first tweet
+	    self.model = LdaModel(corpus=[bow])
 
     def loadModel(self):
         pass
-
 
     def saveModel(self, path):
         pass
