937bc436f7bf7e47da320621afcaff095d921dae
Chris Jepeway <jepeway@blasted-heath.com>
2015-12-16 09:02:13 -0500

    Let remote classifier be silent

--- python/classifier/remote.py
+++ python/classifier/remote.py
@@ -6,18 +6,19 @@ class RemoteTweetClassifier(TweetClassifier):
     """
     Create a proxy to a remote TweetClassifier
     """
-    def __init__(self, endpt):
+    def __init__(self, endpt, silent=False):
 	self.proxy = None	# Set to None so closures over an instance will know to create the proxy
 	self.endpt = endpt
+	self.silent = silent
 
     def isHarassingTweet(self, txt):
-	print("checking: " + txt)
+	if not self.silent: print("checking: " + txt)
 	if self.proxy is None:
 	    self.proxy = xmlrpclib.ServerProxy(self.endpt, allow_none = True)
         return self.proxy.isHarassingTweet(txt)
 
     def addHarassingTweet(self, txt):
-	print("  adding: " + txt)
+	if not self.silent: print("  adding: " + txt)
 	if self.proxy is None:
 	    self.proxy = xmlrpclib.ServerProxy(self.endpt, allow_none = True)
         self.proxy.addHarassingTweet(txt)
